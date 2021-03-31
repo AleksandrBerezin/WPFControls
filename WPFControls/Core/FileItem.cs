dcfd813dd4a34ea4ab79b2e56ea43da5b5c6aa19
AleksandrBerezin <berezin_s3@amil.ru>
2021-03-31 13:15:16 +0700

    feature(WPFControl): реалиховано подсвечивание файлов красным фоном, если расширение не .exe или .dll

--- WPFControls/Core/FileItem.cs
+++ WPFControls/Core/FileItem.cs
@@ -1,4 +1,6 @@
-﻿namespace Core
+﻿using System.IO;
+
+namespace Core
 {
     /// <summary>
     /// Класс <see cref="FileItem"/> хранит данные о файле
@@ -6,9 +8,28 @@
     public class FileItem
     {
         /// <summary>
+        /// Имя файла
+        /// </summary>
+        private string _name;
+
+        /// <summary>
         /// Возвращает и задает имя файла
         /// </summary>
-        public string Name { get; set; }
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                var extension = Path.GetExtension(_name);
+                IsValidName = extension == ".exe" || extension == ".dll";
+            }
+        }
+
+        /// <summary>
+        /// Возрращает true, если файл имеет расширение *.exe или *.dll
+        /// </summary>
+        public bool IsValidName = true;
 
         /// <summary>
         /// Создает экземпляр <see cref="FileItem"/>
---
+++ WPFControls/WPFControls/FileStyleSelector.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Core;
+
+namespace WPFControls
+{
+    /// <summary>
+    /// Селектор стиля <see cref="FileStyleSelector"/> для элемента списка файлов
+    /// </summary>
+    public class FileStyleSelector : StyleSelector
+    {
+        /// <summary>
+        /// Стиль для расширения файлов .exe или .dll
+        /// </summary>
+        public Style CorrectExtensionStyle { get; set; }
+
+        /// <summary>
+        /// Стиль для всех расширений файлов, кроме .exe или .dll
+        /// </summary>
+        public Style IncorrectExtensionStyle { get; set; }
+
+        /// <inheritdoc/>
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+            var file = (FileItem) item;
+            return file.IsValidName ? CorrectExtensionStyle : IncorrectExtensionStyle;
+        }
+    }
+}
