f1d7251d07b9e053f8345049a0f948d95c15a3fe
AleksandrBerezin <berezin_s3@amil.ru>
2021-04-15 14:41:20 +0700

    feature(Control): реализован конвертер вместо селектора стилей

---
+++ WPFControls/WPFControls/FileItemToBrushConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using Core;
+
+namespace WPFControls
+{
+    /// <summary>
+    /// Класс <see cref="FileItemToBrushConverter"/> конвертирует <see cref="FileItem"/> в <see cref="SolidColorBrush"/>
+    /// </summary>
+    public class FileItemToBrushConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((FileItem) value).IsValidName
+                ? new SolidColorBrush(Colors.White)
+                : new SolidColorBrush(Color.FromRgb(255, 238, 238));
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
